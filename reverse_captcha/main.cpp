617a169549aa5af60e31a3a2e4ead8bd9b538382
Ben Melikant <bmelikant@dtec.com>
2017-12-04 21:29:38 -0500

    [master] added day one and two second solutions

--- reverse_captcha/main.cpp
+++ reverse_captcha/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 #include <cstdlib>
 
 using namespace std;
 
 int main () {
+
+    string number;
+    getline(cin, number);
+    int cmpDistance = number.length() / 2;
+    int current = 0, compare = 0, index = 0;
+    int captcha = 0;
+
+    while (index < number.length()) {
+
+        current = strtol(number.substr(index, 1).c_str(), NULL, 10);
+        
+        if (index+cmpDistance < number.length()) {
+            compare = strtol(number.substr(index+cmpDistance, 1).c_str(), NULL, 10);
+        } else {
+            int cmpIdx = (index+cmpDistance) - number.length();
+            compare = strtol(number.substr(cmpIdx, 1).c_str(), NULL, 10);
+        }
+
+        cout << "current: " << current << ", compare: " << compare << endl;
+        if (current == compare)
+            captcha += current;
+
+        index++;
+    }
+
+    cout << "The captcha result is " << captcha << endl;
+}
+
+void reverse_captcha () {
 
     int startnum = 0, current = 0, next = 0;
     int sum = 0;
--- spreadsheet_checksum/main.cpp
+++ spreadsheet_checksum/main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
 int main () {
+
+    int checksum = 0;
+
+    while (!cin.eof()) {
+
+        string line;
+        getline (cin, line);
+        istringstream iss(line);
+
+        vector<int> lineIntegers;
+        int current = 0;
+
+        while (iss >> current) {
+            lineIntegers.push_back(current);
+        }
+
+        for (size_t i = 0; i < lineIntegers.size(); i++) {
+
+            for (int j = 0; j < lineIntegers.size(); j++) {
+                if (lineIntegers.at(i) > lineIntegers.at(j) && (lineIntegers.at(i) % lineIntegers.at(j)) == 0) {
+                    checksum += (lineIntegers.at(i) / lineIntegers.at(j));
+                    cout << "Current checksum: " << checksum << endl;
+                }
+            }
+        }
+    }
+
+    cout << "The checksum value is " << checksum << endl;
+}
+
+void spreadsheet_checksum () {
+
 
     int checksum = 0;
 
